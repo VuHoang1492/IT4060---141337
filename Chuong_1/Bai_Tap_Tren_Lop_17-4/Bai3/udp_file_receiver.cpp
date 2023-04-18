0e0e97f84059608e3296e97b1e9986ebae52988a
VŨ ĐỨC HOÀNG <Hoangbarca1492@gmail.com>
2023-04-18 18:58:29 +0700

    Bai 3"

---
+++ Chuong_1/Bai_Tap_Tren_Lop_17-4/Bai3/udp_file_receiver.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int receiver = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(atoi(argv[1]));
+
+    int b = bind(receiver, (struct sockaddr *)&addr, sizeof(addr));
+    if (b == -1)
+    {
+        cout << "bind failed\n";
+        return 0;
+    }
+
+    char buf[1024];
+    char name[100];
+    int name_len = recvfrom(receiver, name, sizeof(name), 0, NULL, NULL);
+    name[name_len] = '\0';
+
+    string s = "receive-";
+    s += name;
+
+    fstream file;
+    file.open(s, ios::out);
+    if (!file.is_open())
+    {
+        cout << "open file failed!\n";
+        return 0;
+    }
+    while (true)
+    {
+        int ret = recvfrom(receiver, buf, sizeof(buf), 0, NULL, NULL);
+        if (ret <= 0)
+        {
+            break;
+        }
+        buf[ret] = '\0';
+        file << buf << endl;
+    }
+
+    file.close();
+}
---
+++ Chuong_1/Bai_Tap_Tren_Lop_17-4/Bai3/udp_file_sender.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int sender = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(argv[1]);
+    addr.sin_port = htons(atoi(argv[2]));
+
+    char mes[1024];
+
+    char *name = argv[3];
+    sendto(sender, name, strlen(name), 0, (struct sockaddr *)&addr, sizeof(addr));
+
+    fstream file;
+    file.open(argv[3], ios::in);
+    if (!file.is_open())
+    {
+        cout << "Open file failed!\n";
+        return 0;
+    }
+    cout << "Send data in file \n";
+    while (file.getline(mes, 1024))
+    {
+        int ret = sendto(sender, mes, strlen(mes), 0, (struct sockaddr *)&addr, sizeof(addr));
+    }
+    file.close();
+}
